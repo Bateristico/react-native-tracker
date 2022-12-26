372fde9d8f67e208dc49320c8ce2a53cd00f81bb
Bateristico <camilo.saa@gmail.com>
2022-12-26 17:28:50 +0100

    add useState for email, password and avoid keyboard view

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Spacer>
-        <Input label="Email" />
-      </Spacer>
-      <Spacer>
-        <Input label="Password" />
-      </Spacer>
-      <Spacer>
-        <Button title="Sign Up" />
-      </Spacer>
-    </View>
+    <KeyboardAvoidingView>
+      <ScrollView contentContainerStyle={styles.contentContainerStyle}>
+        <View style={styles.container}>
+          <Spacer>
+            <Text h3>Sign Up for Tracker</Text>
+          </Spacer>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            label="Email"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <Spacer />
+          <Input
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+          />
+          <Spacer />
+          <Spacer>
+            <Button title="Sign Up" />
+          </Spacer>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -33,6 +51,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250
+  },
+  contentContainerStyle: {
+    paddingVertical: 150
   }
 });
 
