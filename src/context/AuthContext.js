3463273089be8cc5b78c597316ba092ae50a1743
Bateristico <camilo.saa@gmail.com>
2023-01-08 20:54:35 +0100

    add boilerplate context function for signin, signout and signup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,4 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
-export const { Provider, Context } = createDataContext(authReducer, {}, { isSignedIn: false });
+const signup = dispatch => {
+  return ({ email, password }) => {
+    // make api request to signup
+    // then update our state as authenticated
+    // if it fails, return the error
+  };
+};
+
+const signin = dispatch => {
+  return ({ email, password }) => {
+    // make api request
+    // update state
+    // handle error
+  };
+};
+
+const signout = dispatch => {
+  return () => {
+    //signout
+  };
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signin, signup, signout },
+  { isSignedIn: false }
+);
